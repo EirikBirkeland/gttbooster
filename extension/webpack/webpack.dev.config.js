18cf3949fcb98e611328c288a35596c2cca313f4
Eirik Birkeland <birketrans@gmail.com>
2017-12-08 21:48:59 +0100

    omg

--- extension/webpack/webpack.dev.config.js
+++ extension/webpack/webpack.dev.config.js
@@ -8,7 +8,7 @@ module.exports = mergeDeep(base, {
   },
   plugins: [
     new WebpackShellPlugin({
-      onBuildStart: ['echo "Starting DEV"', 'which philipsHue.sh && philipsHue.sh --number 4 --color red'],
+      onBuildStart: ['echo "Starting DEV"'/*, 'which philipsHue.sh && philipsHue.sh --number 4 --color red'*/],
       onBuildExit: ['bin/npm-start.sh']
 })
   ],
