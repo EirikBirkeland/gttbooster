ae5db562f495e766e1cb8f214be735ad8fb22c42
Eirik Birkeland <birketrans@gmail.com>
2017-12-08 21:29:36 +0100

    .

--- extension/bin/npm-build.sh
+++ extension/bin/npm-build.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
-philipsHue.sh --number 4 --color red
+
+function hue {
+   which philipsHue.sh
+   if [ $? -eq 0 ]; then
+     philipsHue.sh --number $1 --color $2
+   else
+     echo "PhilipsHue.sh not available."
+   fi
+}
+
+hue 4 red
 
 printf "RUNNING GULP P\n\n"
 gulp p
 cd dist
 rm -Rf ./archive
 unzip -q archive.zip -d archive
-philipsHue.sh --number 4 --color yellow
+
+hue 4 yellow
 
 npm run test
 if [ $? -eq 0 ]; then
     # green light if the command returned exit status 0
-    philipsHue.sh --color green --number 4
+    hue 4 green
 else
     # red light if the command returned 1 or sth else
-    philipsHue.sh --color red --number 4
+    hue 4 red
 fi
 
 # Check for any GPL that might have infected my codebase
