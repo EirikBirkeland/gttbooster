cd97dedf1defefbd70dbef4c62d8466f539303f7
Eirik Birkeland <birketrans@gmail.com>
2018-10-09 20:40:16 +0200

    Add necessary initialization timer

---
+++ extension/src/workbench/4_controller/emitters/other-index.js
@@ -0,0 +1,19 @@
+// Copyright © 2017 Eirik Birkeland. All rights reserved.
+/**
+ * Created by eb on 15.07.2017.
+ */
+
+import addExtraXRegExp from '../../2_classes/xreg';
+import * as Hotkeys from '../../2_classes/Hotkeys';
+
+function init (options) {
+   addExtraXRegExp();
+
+   if (options.addShortcutCtrlB) {
+      setTimeout(()=>{
+      Hotkeys.hideTopMenusAndTm();
+      }, 5000)
+   }
+}
+
+export { init };
