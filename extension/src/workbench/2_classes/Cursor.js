ca98941512dc6bd3bb9e70b59da45517b2121522
Eirik Birkeland <birketrans@gmail.com>
2018-11-02 14:35:44 +0100

    Further revert

--- extension/src/workbench/2_classes/Cursor.js
+++ extension/src/workbench/2_classes/Cursor.js
@@ -3,13 +3,13 @@ import $ from 'jquery';
 
 const resetLoadIndication = () => {
    $(document.body).css('cursor', 'default')
-   $(window.cth.dom.sourceDoc.body).css('cursor', 'default')
-   $(window.cth.dom.targetDoc.body).find('*').css('cursor', 'default')
+   // $(window.cth.dom.sourceDoc.body).css('cursor', 'default')
+   // $(window.cth.dom.targetDoc.body).find('*').css('cursor', 'default')
 };
 const showLoadIndication = () => {
    $(document.body).css('cursor', 'progress')
-   $(window.cth.dom.sourceDoc.body).css('cursor', 'progress')
-   $(window.cth.dom.targetDoc.body).css('cursor', 'progress')
+   // $(window.cth.dom.sourceDoc.body).css('cursor', 'progress')
+   // $(window.cth.dom.targetDoc.body).css('cursor', 'progress')
 };
 
 export const Cursor = {
